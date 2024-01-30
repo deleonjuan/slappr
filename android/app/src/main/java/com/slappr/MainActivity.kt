6cb1718dddc471cc8046de834f163d76e289b3e5
deleonjuan <juan.deleon.dev@gmail.com>
2024-01-29 21:25:44 -0600

    fix: minur issue w/ react-native-screens fixed

--- android/app/src/main/java/com/slappr/MainActivity.kt
+++ android/app/src/main/java/com/slappr/MainActivity.kt
@@ -13,6 +13,11 @@ class MainActivity : ReactActivity() {
    */
   override fun getMainComponentName(): String = "slappr"
 
+  //react-native-screens override
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null);
+  }
+
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
